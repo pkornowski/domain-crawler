44570935714a66bc3f3a0c3f9526a5aed7b29088
Przemyslaw Kornowski <przemek.kornowski@gmail.com>
2018-10-22 20:51:18 +0200

    Added jsoup dependency for parsing raw HTML documents.

--- src/main/java/com/domaincrawler/crawler/CrawlerApplication.java
+++ src/main/java/com/domaincrawler/crawler/CrawlerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public classCrawlerApplication {
+public class CrawlerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlerApplication.class, args);
--- src/main/java/com/domaincrawler/crawler/manager/CrawlerManagerImpl.java
+++ src/main/java/com/domaincrawler/crawler/manager/CrawlerManagerImpl.java
@@ -1,10 +1,16 @@
 package com.domaincrawler.crawler.manager;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashSet;
+
+import static org.springframework.http.HttpHeaders.USER_AGENT;
 
 @Component
 public class CrawlerManagerImpl implements CrawlerManager {
@@ -15,7 +21,15 @@ public class CrawlerManagerImpl implements CrawlerManager {
 
     @Override
     public HashSet<String> listAllDetectedUrls(final URL url) throws IOException {
+        Connection connection = Jsoup.connect(url.toString()).userAgent(USER_AGENT);
+        Document mainDocument = connection.get();
+        crawlDocument(mainDocument);
         return new HashSet<>();
     }
 
+    private void crawlDocument(final Document document) {
+        Elements links = document.select("a[href]");
+        Elements media = document.select("[src]");
+        Elements imports = document.select("link[href]");
+    }
 }
