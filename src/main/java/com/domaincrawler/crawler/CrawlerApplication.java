0eecf1e611727ebe4842ed57c7a5d3d4ca7d35ad
Przemyslaw Kornowski <przemek.kornowski@gmail.com>
2018-10-22 20:45:37 +0200

    Init Spring-Boot app.

---
+++ src/main/java/com/domaincrawler/crawler/CrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.domaincrawler.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public classCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+}
